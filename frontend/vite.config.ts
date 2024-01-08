88adf3e908f2da26eaf9c509c9dc9217b755942d
Shish <shish@shishnet.org>
2024-01-08 11:28:02 +0000

    [frontend] force ipv4 to connect to backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/graphql': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         secure: false,
         ws: true,
